737d469b75e315ae555711e5985211f9157508dd
sara-k03 <sarayu.kondaveeti@gmail.com>
2025-03-05 14:28:45 -0500

    PCD analysis

---
+++ pcd_analysis.py
@@ -0,0 +1,30 @@
+import numpy as np 
+import open3d as o3d
+import plotly.graph_objects as go
+
+# reads point cloud from pcd
+# change the file name for where your data is located
+pcd = o3d.io.read_point_cloud("/Users/sarayukondaveeti/Desktop/point_cloud_modeling/Datasets/point_cloud_data.pcd")
+#stores point clouds in array
+out_arr = np.asarray(pcd.points)  
+
+# 3D Scatter Plot
+fig = go.Figure( data = [ go.Scatter3d (
+    x = out_arr[:, 0],  # X-coordinates
+    y = out_arr[:, 1],  # Y-coordinates
+    z = out_arr[:, 2],  # Z-coordinates
+    mode = 'markers',
+    marker = dict(size=2, color=out_arr[:, 2], colorscale='Viridis', opacity=0.8)
+)])
+
+# Labels & rotation
+fig.update_layout(
+    scene=dict(
+        xaxis_title='X',
+        yaxis_title='Y',
+        zaxis_title='Z'
+    ),
+    title="Interactive 3D Point Cloud",
+)
+
+fig.show()
